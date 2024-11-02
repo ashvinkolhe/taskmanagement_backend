4d7665c9d6ca8ae932382ddbe6f71d8982a521e1
Ashwin Kolhe <ashwinkolhe09@gmail.com>
2024-11-02 11:25:39 +0530

    Added CORS support to Express server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const taskRoutes = require('./routes/taskRoutes');
-const cors = require('cors'); 
+const cors = require('cors');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -10,7 +10,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+// CORS Configuration
+const corsOptions = {
+  origin: 'http://localhost:5173', // Allow your frontend origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json()); // Parse JSON request body
 app.use('/api/users', userRoutes); // User routes
